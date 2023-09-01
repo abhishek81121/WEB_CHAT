19127570c981efd34578126e6829ed6ada32398b
Abhishek Kumar Singh <abhishek81121@gmail.com>
2023-09-01 23:35:31 +0530

    initialized firebase

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,88 +1,25 @@
 "use client";
-import { useState } from "react";
 import styles from "./page.module.css";
 import { motion } from "framer-motion";
-import axios from "axios";
-export default function chat() {
-  const [visible, setVisible] = useState("none");
-  const [btntxt, setBtntxt] = useState("Create Room");
-  const [roomname, setRoomname] = useState("");
-  const [password, setPassword] = useState("");
-  async function createuser() {
-    axios
-      .post("/api/room", {
-        roomname: roomname,
-        password: password,
-      })
-      .then((response) => {
-        if (response.data.status == "true") {
-          console.log("room created");
-        } else {
-          console.log("room already exist");
-        }
-      });
-  }
+export default function () {
   return (
-    <>
-      {" "}
-      <div
-        className={styles.body}
-        onClick={() => {
-          setVisible(() => "flex");
-        }}
-      >
-        <div className={styles.button}>
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            className={styles.buttonin}
-            onClick={() => {
-              setBtntxt(() => "Create Room");
-            }}
-          >
-            New Room
-          </motion.button>
-        </div>
-        <div className={styles.button}>
-          {" "}
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            className={styles.buttonin}
-            onClick={() => {
-              setBtntxt(() => "Join Room");
-            }}
-          >
-            Join Room
-          </motion.button>
-        </div>
-      </div>
-      <div className={styles.popup} style={{ display: visible }}>
-        <div className={styles.popupbody}>
+    <div className={styles.main}>
+      <div className={styles.contacts}>
+        <div className={styles.friendlist}></div>
+        <div className={styles.addfriend}>
           <motion.input
-            whileHover={{ scale: 1.05 }}
-            className={styles.roomname}
             type="text"
-            placeholder="Room Name"
-            onChange={(e) => setRoomname(() => e.target.value)}
+            className={styles.friendsername}
+            whileHover={{ scale: 1.02 }}
           ></motion.input>
-          <motion.input
-            whileHover={{ scale: 1.05 }}
-            className={styles.roompassword}
-            placeholder="Password"
-            type="password"
-            onChange={(e) => setPassword(() => e.target.value)}
-          ></motion.input>
-          <motion.button
-            className={styles.createbutton}
-            whileHover={{ scale: 1.05 }}
-            onClick={() => {
-              setVisible(() => "none");
-              createuser();
-            }}
-          >
-            {btntxt}
+          <motion.button className={styles.find} whileHover={{ scale: 1.07 }}>
+            +
           </motion.button>
         </div>
       </div>
-    </>
+      <div className={styles.chat}>
+        <div className={styles.message}></div>
+      </div>
+    </div>
   );
 }
